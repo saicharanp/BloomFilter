4ae3d5144b0c93aad92bf0d2e23d768fc82c0ebe
Saicharan Poduri <sapoduri@microsoft.com>
2018-11-19 11:41:40 -0800

    Wrap component in Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import CustomAppBar from './components/CustomAppBar';
-import HeroUnit  from './components/HeroUnit';
-import Footer from './components/Footer';
-import GridContainer from './components/GridContainer';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import CustomAppBar from "./components/CustomAppBar";
+import HeroUnit from "./components/HeroUnit";
+import Footer from "./components/Footer";
+import GridContainer from "./components/GridContainer";
+import CssBaseline from "@material-ui/core/CssBaseline";
 class App extends Component {
-  state = {users: []}
-  
+  state = { users: [] };
+
   componentDidMount() {
     // fetch('/users')
     //   .then(res => res.json())
@@ -16,20 +16,19 @@ class App extends Component {
   }
 
   render() {
-      
-      return (
-        <Provider store={store}>
-          <React.Fragment>
-            <CssBaseline />
-            <CustomAppBar />
-            <main>
-              <HeroUnit />
-              <GridContainer />
-            </main>
-            <Footer />
-          </React.Fragment>
-        </Provider>
-      );
+    return (
+      <Provider store={store}>
+        <React.Fragment>
+          <CssBaseline />
+          <CustomAppBar />
+          <main>
+            <HeroUnit />
+            <GridContainer />
+          </main>
+          <Footer />
+        </React.Fragment>
+      </Provider>
+    );
   }
 }
 
